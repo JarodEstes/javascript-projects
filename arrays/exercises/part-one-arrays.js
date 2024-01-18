9ce37b23e86ec8f319e95700ddda5c6bb9b3bcd6
JarodEstes <estesjarod48@gmail.com>
2024-01-18 17:57:32 -0600

    array

--- arrays/exercises/part-one-arrays.js
+++ arrays/exercises/part-one-arrays.js
@@ -1,9 +1,8 @@
 //Create an array called practiceFile with the following entry: 273.15
-
+let practiceFile = [273.15];
 //Use the bracket notation method to add "42" and "hello" to the array. Add these new items one at a time.  Print the array after each step to confirm the changes.
-
+practiceFile.push(42, "hello");
+console.log(practiceFile);
 //Use a single .push() to add the following items: false, -4.6, and "87".  Print the array to confirm the changes.
-let classes = ["science, computer, art"];
-console.log(classes.length);
-let teachers = ["Jones", "Willoughby", "Rhodes"];
-console.log(teachers.length);
+practiceFile.push(false, -4.6, "87");
+console.log(practiceFile);
