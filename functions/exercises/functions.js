3a5f1b7386fc2cdbfc47f527e1ed56e366660148
JarodEstes <estesjarod48@gmail.com>
2024-01-26 00:03:53 -0600

    function exercises

--- functions/exercises/functions.js
+++ functions/exercises/functions.js
@@ -6,3 +6,21 @@ function makeLine(size) {
     return line;
   }
   console.log(makeLine(5));
+
+  function makeSquare(size) {
+    let line = '';
+    for (let i = 0; i < size; i++); {
+        line += makeLine(size)+ '\n'
+    }
+    return line;
+  }
+  console.log(makeSquare(5));
+
+//   function makeRectangle(width, height) {
+//     let rectangle = '';
+//     for (let i = 0; i < height; i++) {
+//       rectangle += (+ makeLine(width) + '\n');
+//     }
+//     return rectangle.slice(0, -1);
+//   }
+//   console.log(makeRectangle(5, 3));
