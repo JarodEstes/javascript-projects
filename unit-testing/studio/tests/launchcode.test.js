78d513a100eed44ed27a29a0689e1f37737ce3a0
JarodEstes <estesjarod48@gmail.com>
2024-02-08 20:51:39 -0600

    unit-testing studio

--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -2,7 +2,14 @@
 const launchcode = require('../index.js');
 
 describe("Testing launchcode", function(){
-
   // Write your unit tests here!
-  
+  test('Returns value as "nonprofit" for the key organization',function(){
+    expect(launchcode.organization).toBe("nonprofit")
+  })
+  test('Returns value as "nonprofit" for the key organization',function(){
+    expect(launchcode.executiveDirector).toBe('Jeff')
+  })
+  test('Returns value as "nonprofit" for the key organization',function (){
+    expect(launchcode.percentageCoolEmployees).toBe(100)
+  })
 });
