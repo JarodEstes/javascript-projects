5a8cd092ea0c4fd26fc40022370a45bb4755c81c
JarodEstes <estesjarod48@gmail.com>
2024-03-04 20:32:00 -0600

    Fetch Studios

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -1,1 +1,18 @@
 //TODO: Add Your Code Below
+window.addEventListener('load', function() {
+    fetch('https://handlers.education.launchcode.org/static/astronauts.json').then(res)
+
+    const container = document.getElementById("container");
+    for (let i = 0; i < JSON.length; i++) {
+        container.innerHTML = `
+        <div class="astronaut">
+        <div class="bio">
+            <h3>${json[i].firstName} ${json[i].lastName}</h3>
+            <ul>
+            <li>Skills; Physician, Chemical Engineer</li>
+            </ul>
+        </div>
+        <img class ="avatar" src="images/mac-jenison.jpg">
+        `;
+    }
+})
